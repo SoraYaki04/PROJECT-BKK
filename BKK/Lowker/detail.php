a117b0782562f139283c17121bbebcd83531aad9
revamewing <reppalistaa7@gmail.com>
2025-01-15 13:56:47 +0700

    Add files via upload

---
+++ BKK/Lowker/detail.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bursa Kerja Khusus SMKN 1 Boyolangu</title>
+  <link rel="stylesheet" href="detail.css?v=<?php echo time(); ?>">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+</head>
+<body>
+
+  <header>
+    <div class="rectangle">
+    </div>
+
+    <div class="logo">
+      <div class="logo-header">
+        <img src="../logo.png" alt="BKK SMKN 1 Boyolangu Logo">
+      </div>
+      <div class="header-text">
+          <img src="../tulisan logo.png" alt="Bursa Kerja Khusus SMKN 1 Boyolangu" id="text-img">
+      </div>
+    </div>
+
+    <div class="contact-info">
+      <div class="contact-phone">
+        <i class="fas fa-phone-alt"></i>
+        <div class="contact-ket">
+          <p>Call Us</p>
+          <p>+6281-xxx-xxx-xxx</p>
+        </div>
+      </div>
+      <div class="contact-email">
+        <i class="fas fa-envelope"></i>
+        <div class="contact-ket">
+          <p>Email Us</p>
+          <p>bkk@smkn1boyolangu@gmail.com</p>
+        </div>
+
+      </div>
+      <div class="contact-map">
+        <i class="fas fa-map-marker-alt"></i>
+        <div class="contact-ket">
+          <p>Located Us</p>
+          <p>Jl. Ki Mangun Sarkoro No.VI/3, Beji, Boyolangu</p>
+        </div>
+
+      </div>
+    </div>
+  </header>
+
+  <div class="container"> 
+    <nav class="navbar">
+      <!-- data-feather="chevron-down"> -->
+       <ul class="navbar-container">
+          <li>
+            <a href="index.html">HOME<i class="fa-solid fa-chevron-down"></i></a>
+            <ul class="dropdown">
+              <li><a href="index.html">Halaman Utama</a></li>
+              <li><a href="pengantar.html">Pengantar</a></li>
+              <li><a href="">Informasi Kegiatan BKK</a></li>
+              <li><a href="">Rekapitulasi</a></li>
+            </ul>
+          </li>
+
+        <li><a href="#">TENTANG KAMI<i class="fa-solid fa-chevron-down"></i></a>
+        <ul class="dropdown">
+          <li><a href="visimisi.html">Visi Misi</a></li>
+          <li><a href="proker.html">Progam Kerja BKK</a></li>
+          <li><a href="tujuan.html">Tujuan</a></li>
+          <li><a href="strukturorganisasi.html">Struktur Organisasi</a></li>
+        </ul>
+        </li>
+        
+        <li><a href="#">LOGIN<i class="fa-solid fa-chevron-down"></i></a>
+        <ul class="dropdown">
+          <li><a href="admin-login.html">Admin BKK</a></li>
+          <li><a href="management-login.html">Management</a></li>
+          <li><a href="siswa-alumni-login.html">Siswa / Alumni</a></li>
+          <li><a href="pengguna-lain-login.html">Partisipan Lain</a></li>
+        </ul>
+        </li>
+
+        <li><a href="informasiJurusan.html">INFORMASI JURUSAN</a></li>
+        <li><a href="perusahaan.html">PERUSAHAAN</a></li>
+        <li><a href="loker.html" class="active">LOWONGAN KERJA</a></li>
+       </ul>
+
+        
+    </nav>
+    
+    <div class="header-bar">
+        <a href="#">Lowongan Kerja</a>
+    </div>
+    <main>
+        <section class="detail-lowongan">
+            <div class="container-detail">
+              <div class="kotak-satu">
+                <h2>BACK END DEVELOPER (PHP/LARAVEL)</h2>
+                <div class="rincian">
+                  <ul>
+                    <li><i class="fa-solid fa-building"></i>UD.BINTANG</li>
+                    <li><i class="fa-solid fa-location-dot"></i>Malang</li>
+                    <li><i class="fa-solid fa-users"></i></i>35 pelamar</li>
+                  </ul>
+                </div>
+
+                <p><strong>UD.BINTANG</strong> merupakan layanan berbasis software saat ini membutuhkan Back End Developer (PHP/Laravel) yang bertugas untuk merancang, membuat dan mengelola aplikasi web serta berkontribusi dalam pengembangan aplikasi web dengan kemampuan technical yang dimiliki.</p>
+
+                <div class="gaji">
+                    <p>Gaji :</p>
+                    <p>Rp 3-4 juta</p>
+                </div>
+
+                <div class="pendidikan">
+                    <p>Pendidikan :</p>
+                    <p>Diploma D1/D2/D3, Sarjana S1, SMA/SMK</p>
+                </div>
+
+                <div class="tipe">
+                  <p>Tipe pekerjaan :</p>
+                  <p>Full time, remote (work from anywhere)</p>
+                </div>
+
+                <div class="tanggal">
+                  <p>Tanggal posting : Kamis,18 Nov 2021</p>
+                  <p>Tanggal exp : Sabtu, 18 Des 2021</p>
+                </div>
+            </div>
+              
+              <div class="kotak-dua">
+                <div class="detail-content">
+                    <div class="info">
+                        <h3>Keahlian:</h3>
+                        <ul>
+                            <li>PHP</li>
+                            <li>MySQL/MariaDB/PostgreSQL</li>
+                            <li>Backend framework: CodeIgniter/Laravel/Slim</li>
+                            <li>REST API, JSON</li>
+                            <li>Familiar dengan layanan AWS: SQS, SES</li>
+                            <li>GIT</li>
+                        </ul>
+
+                        <h3>Waktu Bekerja:</h3>
+                        <p>Senin–Jumat, 08.00–17.00 WIB</p>
+                    </div>
+
+                    <div class="requirements">
+                        <h3>Kualifikasi:</h3>
+                        <ul>
+                            <li>Pria/Wanita</li>
+                            <li>Pendidikan minimal SMA/SMK</li>
+                            <li>Dapat bekerja dengan tim</li>
+                            <li>Bersedia bekerja dengan sistem remote</li>
+                        </ul>
+
+                        <h3>Tunjangan:</h3>
+                        <ul>
+                            <li>Tunjangan Komunikasi</li>
+                            <li>BPJS Kesehatan</li>
+                            <li>BPJS Ketenagakerjaan</li>
+                            <li>Cuti Tahunan</li>
+                            <li>THR</li>
+                        </ul>
+                    </div>
+                </div>
+              </div>
+
+                <div class="actions">
+                    <button class="save-btn">SIMPAN</button>
+                    <button class="apply-btn">LAMAR</button>
+                </div>
+            </div>
+        </section>
+    </main>
