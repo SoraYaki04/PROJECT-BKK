4e2b46fad36004825d425592884b1a132dfd3c66
revamewing <reppalistaa7@gmail.com>
2025-02-12 22:51:27 +0700

    Add files via upload

---
+++ BKK/Home/info-keg.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Info Kegiatan BKK</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;800&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link href="info-keg.css?v=<?php echo time(); ?>" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="rectangle">
+        </div>
+        </div>
+
+        <div class="logo">
+            <div class="logo-header">
+              <img src= "../logo.png" alt="BKK SMKN 1 Boyolangu Logo">
+            </div>
+            <div class="header-text">
+                <img src="../tulisan logo.png" alt="Bursa Kerja Khusus SMKN 1 Boyolangu" id="text-img">
+            </div>
+        </div>
+
+        <div class="contact-info">
+            <div class="contact-phone">
+              <i class="fas fa-phone-alt"></i>
+              <div class="contact-ket">
+                <p>Call Us</p>
+                <p>+6281-xxx-xxx-xxx</p>
+              </div>
+            </div>
+            <div class="contact-email">
+              <i class="fas fa-envelope"></i>
+              <div class="contact-ket">
+                <p>Email Us</p>
+                <p>bkk@smkn1boyolangu@gmail.com</p>
+              </div>
+      
+            </div>
+            <div class="contact-map">
+              <i class="fas fa-map-marker-alt"></i>
+              <div class="contact-ket">
+                <p>Located Us</p>
+                <p>Jl. Ki Mangun Sarkoro No.VI/3, Beji, Boyolangu</p>
+              </div>
+      
+            </div>
+        </div>
+     </header>
+      
+    <div class="container"> 
+        <nav class="navbar">
+          <!-- data-feather="chevron-down"> -->
+           <ul class="navbar-container">
+            <li>
+              <a href="#" class="active">HOME<i class="fa-solid fa-chevron-down"></i></a>
+              <ul class="dropdown">
+                <li><a href="index.html">Halaman Utama</a></li>
+                <li><a href="pengantar.html" class="active">Pengantar</a></li>
+                <li><a href="">Informasi Kegiatan BKK</a></li>
+                <li><a href="">Rekapitulasi</a></li>
+              </ul>
+            </li>
+    
+            <li><a href="#">TENTANG KAMI<i class="fa-solid fa-chevron-down"></i></a>
+            <ul class="dropdown">
+              <li><a href="visimisi.html">Visi Misi</a></li>
+              <li><a href="proker.html">Progam Kerja BKK</a></li>
+              <li><a href="tujuan.html">Tujuan</a></li>
+              <li><a href="strukturorganisasi.html">Struktur Organisasi</a></li>
+            </ul>
+            </li>
+            
+            <li><a href="#">LOGIN<i class="fa-solid fa-chevron-down"></i></a>
+            <ul class="dropdown">
+                <li><a href="admin-login.html">Admin BKK</a></li>
+                <li><a href="management-login.html">Management</a></li>
+                <li><a href="siswa-alumni-login.html">Siswa / Alumni</a></li>
+                <li><a href="pengguna-lain-login.html">Partisipan Lain</a></li>
+            </ul>
+            </li>
+    
+            <li><a href="informasiJurusan.html">INFORMASI JURUSAN</a></li>
+            <li><a href="perusahaan.html">PERUSAHAAN</a></li>
+            <li><a href="loker.html">LOWONGAN KERJA</a></li>
+           </ul>
+    
+            
+        </nav>
+
+    <div class="container">
+        <div class="header-bar">
+            <a href="#">kegiatan BKK</a>
+    </div>
+
+    <div class="card-container">
+        <div class="card">
+            <div class="title">KUNJUNGAN INDUSTRI</div>
+            <div class="card-content">
+                <div class="foto">
+                    <img src="Kunjungan.jpeg" alt="Kunjungan Industri">
+                </div>
+                <div class="deskripsi">
+                    <hr>
+                    <p>Kegiatan rutin yang dilakukan oleh SMKN 1 Boyolangu untuk mengajak siswa mengunjungi perusahaan-perusahaan besar di sekitar wilayah Jawa Timur.</p>
+                    <button class="read-more-btn">Baca Selengkapnya...</button>
+                </div>
+            </div>
+        </div>
+    
+        <div class="card">
+            <div class="title">KUNJUNGAN INDUSTRI</div>
+            <div class="card-content">
+                <div class="foto">
+                    <img src="Kunjungan.jpeg" alt="Kunjungan Industri">
+                </div>
+                <div class="deskripsi">
+                    <hr>
+                    <p>Kegiatan rutin yang dilakukan oleh SMKN 1 Boyolangu untuk mengajak siswa mengunjungi perusahaan-perusahaan besar di sekitar wilayah Jawa Timur.</p>
+                    <button class="read-more-btn">Baca Selengkapnya...</button>
+                </div>
+            </div>
+        </div>
+    
+        <div class="card">
+            <div class="title">KUNJUNGAN INDUSTRI</div>
+            <div class="card-content">
+                <div class="foto">
+                    <img src="Kunjungan.jpeg" alt="Kunjungan Industri">
+                </div>
+                <div class="deskripsi">
+                    <hr>
+                    <p>Kegiatan rutin yang dilakukan oleh SMKN 1 Boyolangu untuk mengajak siswa mengunjungi perusahaan-perusahaan besar di sekitar wilayah Jawa Timur.</p>
+                    <button class="read-more-btn">Baca Selengkapnya...</button>
+                </div>
+            </div>
+        </div>
+    
+        <div class="card">
+            <div class="title">KUNJUNGAN INDUSTRI</div>
+            <div class="card-content">
+                <div class="foto">
+                    <img src="Kunjungan.jpeg" alt="Kunjungan Industri">
+                </div>
+                <div class="deskripsi">
+                    <hr>
+                    <p>Kegiatan rutin yang dilakukan oleh SMKN 1 Boyolangu untuk mengajak siswa mengunjungi perusahaan-perusahaan besar di sekitar wilayah Jawa Timur.</p>
+                    <button class="read-more-btn">Baca Selengkapnya...</button>
+                </div>
+            </div>
+        </div>
+    
+</body>
+</html>
