241f528d1eb6a15fc447148ed4f24746a8a282ae
SoraYaki <pakli8104@gmail.com>
2025-07-10 18:11:49 +0700

    Update informasiJurusan.php

--- BKK/Informasi Jurusan/informasiJurusan.php
+++ BKK/Informasi Jurusan/informasiJurusan.php
@@ -4,50 +4,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Informasi Jurusan</title>
-    <link rel="stylesheet" href="css/infromasiJurusan.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="informasiJurusan.css?v=<?php echo time(); ?>">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;800&display=swap" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 </head>
 <body>
 
     <header>
-        <div class="rectangle">
+      <div class="rectangle">
+      </div>
+  
+      <div class="logo">
+        <div class="logo-header">
+          <img src="../logo.png" alt="BKK SMKN 1 Boyolangu Logo">
         </div>
-
-        <div class="logo">
-            <div class="logo-header">
-              <img src="assets/foto logo.png" alt="BKK SMKN 1 Boyolangu Logo">
-            </div>
-            <div class="header-text">
-                <img src="assets/tulisan logo.png" alt="Bursa Kerja Khusus SMKN 1 Boyolangu" id="text-img">
-            </div>
+        <div class="header-text">
+            <img src="../tulisan logo.png" alt="Bursa Kerja Khusus SMKN 1 Boyolangu" id="text-img">
         </div>
-
-        <div class="contact-info">
-            <div class="contact-phone">
-              <i class="fas fa-phone-alt"></i>
-              <div class="contact-ket">
-                <p>Call Us</p>
-                <p>+6281-xxx-xxx-xxx</p>
-              </div>
-            </div>
-            <div class="contact-email">
-              <i class="fas fa-envelope"></i>
-              <div class="contact-ket">
-                <p>Email Us</p>
-                <p>bkk@smkn1boyolangu@gmail.com</p>
-              </div>
-      
-            </div>
-            <div class="contact-map">
-              <i class="fas fa-map-marker-alt"></i>
-              <div class="contact-ket">
-                <p>Located Us</p>
-                <p>Jl. Ki Mangun Sarkoro No.VI/3, Beji, Boyolangu</p>
-              </div>
-      
-            </div>
+      </div>
+  
+      <div class="contact-info">
+        <div class="contact-phone">
+          <i class="fas fa-phone-alt"></i>
+          <div class="contact-ket">
+            <p>Call Us</p>
+            <p>+6281-xxx-xxx-xxx</p>
+          </div>
         </div>
+        <div class="contact-email">
+          <i class="fas fa-envelope"></i>
+          <div class="contact-ket">
+            <p>Email Us</p>
+            <p>bkk@smkn1boyolangu@gmail.com</p>
+          </div>
+  
+        </div>
+        <div class="contact-map">
+          <i class="fas fa-map-marker-alt"></i>
+          <div class="contact-ket">
+            <p>Located Us</p>
+            <p>Jl. Ki Mangun Sarkoro No.VI/3, Beji, Boyolangu</p>
+          </div>
+  
+        </div>
+      </div>
      </header>
       
     <div class="container"> 
@@ -55,36 +55,36 @@
           <!-- data-feather="chevron-down"> -->
            <ul class="navbar-container">
             <li>
-              <a href="#">HOME<i class="fa-solid fa-chevron-down"></i></a>
+              <a href="../Home/Halaman Utama/berandautama.html">HOME<i class="fa-solid fa-chevron-down"></i></a>
               <ul class="dropdown">
-                <li><a href="index.html">Halaman Utama</a></li>
-                <li><a href="pengantar.html">Pengantar</a></li>
-                <li><a href="">Informasi Kegiatan BKK</a></li>
+                <li><a href="../Home/Halaman Utama/berandautama.html">Halaman Utama</a></li>
+                <li><a href="../Home/Pengantar/pengantar.html">Pengantar</a></li>
+                <li><a href="../Home/Informasi Kegiatan BKK/informasikegiatanbkk.html">Informasi Kegiatan BKK</a></li>
                 <li><a href="">Rekapitulasi</a></li>
               </ul>
             </li>
     
             <li><a href="#">TENTANG KAMI<i class="fa-solid fa-chevron-down"></i></a>
             <ul class="dropdown">
-              <li><a href="visimisi.html">Visi Misi</a></li>
-              <li><a href="proker.html">Progam Kerja BKK</a></li>
-              <li><a href="tujuan.html">Tujuan</a></li>
-              <li><a href="strukturorganisasi.html">Struktur Organisasi</a></li>
+              <li><a href="../Tentang Kami/visimisi.php">Visi Misi</a></li>
+              <li><a href="../Tentang Kami/proker.php">Progam Kerja BKK</a></li>
+              <li><a href="../Tentang Kami/tujuan.php">Tujuan</a></li>
+              <li><a href="../Tentang Kami/strukturorganisasi.php">Struktur Organisasi</a></li>
             </ul>
             </li>
             
             <li><a href="#">LOGIN<i class="fa-solid fa-chevron-down"></i></a>
             <ul class="dropdown">
-              <li><a href="admin-login.html">Admin BKK</a></li>
-              <li><a href="management-login.html">Management</a></li>
-              <li><a href="siswa-alumni-login.html">Siswa / Alumni</a></li>
-              <li><a href="pengguna-lain-login.html">Partisipan Lain</a></li>
+              <li><a href="../Login/admin-login.html">Admin BKK</a></li>
+              <li><a href="../Login/management-login.html">Management</a></li>
+              <li><a href="../Login/siswa-alumni-login.html">Siswa / Alumni</a></li>
+              <li><a href="../Login/pengguna-lain-login.html">Partisipan Lain</a></li>
             </ul>
             </li>
     
-            <li><a href="informasiJurusan.html"  class="active">INFORMASI JURUSAN</a></li>
-            <li><a href="perusahaan.html">PERUSAHAAN</a></li>
-            <li><a href="loker.html">LOWONGAN KERJA</a></li>
+            <li><a href="#"  class="active">INFORMASI JURUSAN</a></li>
+            <li><a href="../Perusahaan/perusahaan.php">PERUSAHAAN</a></li>
+            <li><a href="../Lowker/loker.php">LOWONGAN KERJA</a></li>
            </ul>
     
             
