ce6cd26b8b9517a3ffc91528db53155d8e8c292c
SoraYaki <pakli8104@gmail.com>
2025-07-24 09:48:18 +0700

    Add files via upload

--- BKK/Informasi Jurusan/informasiJurusan.php
+++ BKK/Informasi Jurusan/informasiJurusan.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Informasi Jurusan</title>
+  <link rel="stylesheet" href="../css/navbar.css?v=<?php echo time(); ?>">
   <link rel="stylesheet" href="informasiJurusan.css?v=<?php echo time(); ?>">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;800&display=swap" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
