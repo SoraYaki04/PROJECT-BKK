c9f3ae4d6b01c88a45f3291a53832c2ceb947b0f
SoraYaki <pakli8104@gmail.com>
2025-07-31 15:55:12 +0700

    Add files via upload

--- BKK/config/security.php
+++ BKK/config/security.php
@@ -1,5 +1,4 @@
 <?php
-
 if (session_status() === PHP_SESSION_NONE) {
     session_set_cookie_params([
         'lifetime' => 86400,
@@ -11,17 +10,17 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-// Generate CSRF token
+// ! Generate CSRF token
 if (empty($_SESSION['csrf_token'])) {
     $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
 }
 
-// Fungsi helper CSRF
+// ! Fungsi helper CSRF
 function csrf_field() {
     return '<input type="hidden" name="csrf_token" value="' . $_SESSION['csrf_token'] . '">';
 }
 
-// Validasi CSRF
+// ! Validasi CSRF
 function validate_csrf() {
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (!isset($_POST['csrf_token']) || !hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])) {
