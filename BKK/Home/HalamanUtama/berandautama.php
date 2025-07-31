0ce697585331983da6bf3c2a8f664e5565e9c6ce
SoraYaki <pakli8104@gmail.com>
2025-07-31 15:37:09 +0700

    Update berandautama.php

--- BKK/Home/HalamanUtama/berandautama.php
+++ BKK/Home/HalamanUtama/berandautama.php
@@ -9,54 +9,17 @@ require_once __DIR__ . '/../../config/helpers.php';
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Bursa Kerja Khusus SMKN 1 Boyolangu</title>
+  <link href="../../navbar/navbar.css?v=<?php echo time(); ?>" rel="stylesheet">
+  <link href="berandautama.css?v=<?php echo time(); ?>" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Ponnala&family=Franklin+Demi+Cond&display=swap" rel="stylesheet">
-  <link href="../../navbar/navbar.css?v=<?php echo time(); ?>" rel="stylesheet">
-  <link href="berandautama.css?v=<?php echo time(); ?>" rel="stylesheet">
+
 </head>
 
 <body>
 
-  <header>
-    <div class="rectangle">
-    </div>
-
-    <div class="logo">
-      <div class="logo-header">
-        <img src="../../logo.png" alt="BKK SMKN 1 Boyolangu Logo">
-      </div>
-      <div class="header-text">
-        <img src="../../tulisan logo.png" alt="Bursa Kerja Khusus SMKN 1 Boyolangu" id="text-img">
-      </div>
-    </div>
-
-    <div class="contact-info">
-      <div class="contact-phone">
-        <i class="fas fa-phone-alt"></i>
-        <div class="contact-ket">
-          <p>Call Us</p>
-          <p>+6281-xxx-xxx-xxx</p>
-        </div>
-      </div>
-      <div class="contact-email">
-        <i class="fas fa-envelope"></i>
-        <div class="contact-ket">
-          <p>Email Us</p>
-          <p>bkk@smkn1boyolangu@gmail.com</p>
-        </div>
-
-      </div>
-      <div class="contact-map">
-        <i class="fas fa-map-marker-alt"></i>
-        <div class="contact-ket">
-          <p>Located Us</p>
-          <p>Jl. Ki Mangun Sarkoro No.VI/3, Beji, Boyolangu</p>
-        </div>
-
-      </div>
-    </div>
-  </header>
+  <?php include '../../navbar/header.php' ?>
 
   <div class="container">
 
