0faba2973d365f3d910e9c3002da604c93403f77
Crimson <kurokyun44@gmail.com>
2025-09-26 21:36:48 +0700

    Update alumni.php

--- BKK/navbar/alumni.php
+++ BKK/navbar/alumni.php
@@ -19,7 +19,7 @@ require_once __DIR__ . '/../config/helpers.php';
         <li><a class="<?= nav_active('Home/HalamanUtama/berandautama.php') ?>" href="<?= base_url('Home/HalamanUtama/berandautama.php') ?>">Halaman Utama</a></li>
         <li><a class="<?= nav_active('Home/Pengantar/pengantar.php') ?>" href="<?= base_url('Home/Pengantar/pengantar.php') ?>">Pengantar</a></li>
         <li><a class="<?= nav_active('Home/InformasiKegiatanBKK/informasikegiatanbkk.php') ?>" href="<?= base_url('Home/InformasiKegiatanBKK/informasikegiatanbkk.php') ?>">Informasi Kegiatan BKK</a></li>
-        <li><a href="#">Rekapitulasi</a></li>
+        <li><a class="<?= nav_active('Rekap/rekap-alumni.php') ?>" href="<?= base_url('Rekap/rekap-alumni.php') ?>">Rekapitulasi</a></li>
       </ul>
     </li>
 
@@ -40,15 +40,21 @@ require_once __DIR__ . '/../config/helpers.php';
         <li><a href="<?= base_url('Login/LoginAdmin/admin-login.php') ?>">Admin BKK</a></li>
         <li><a href="<?= base_url('Login/LoginManagement/management-login.php') ?>">Management</a></li>
         <li><a href="<?= base_url('Login/LoginSiswa/siswa-alumni-login.php') ?>">Siswa / Alumni</a></li>
-        <li><a href="<?= base_url('Login/loginrecruiter/recruiter-login.php') ?>">Partisipan Lain</a></li>
+        <li><a href="<?= base_url('Login/LoginUserLain/pengguna-lain-login.html') ?>">Partisipan Lain</a></li>
       </ul>
     </li>
     <?php endif; ?>
 
     <li><a class="<?= nav_active('InformasiJurusan/informasiJurusan.php') ?>" href="<?= base_url('InformasiJurusan/informasiJurusan.php') ?>">INFORMASI JURUSAN</a></li>
     <li><a class="<?= nav_active('Perusahaan/perusahaan.php') ?>" href="<?= base_url('Perusahaan/perusahaan.php') ?>">PERUSAHAAN</a></li>
-    <li><a class="<?= nav_active('Lowker/loker.php') ?>" href="<?= base_url('Lowker/loker.php') ?>">LOWONGAN KERJA</a></li>
+    <li>
+      <a class="<?= nav_active(['Lowker/loker.php', 'Rekap/rekap-loker.php']) ?>" href="#">LOWONGAN KERJA<i class="fa-solid fa-chevron-down"></i></a>
+      <ul class="dropdown">
+        <li><a class="<?= nav_active('Lowker/loker.php') ?>" href="<?= base_url('Lowker/loker.php') ?>">Daftar Lowongan</a></li>
+        <li><a class="<?= nav_active('Rekap/rekap-loker.php') ?>" href="<?= base_url('Rekap/rekap-loker.php') ?>">Rekap Lowongan Kerja</a></li>
+      </ul>
+    </li>
+    <li><a class="<?= nav_active('Survey/survey.php') ?>" href="<?= base_url('Survey/survey.php') ?>">SURVEY</a></li>
 
   </ul>
 </nav>
-
