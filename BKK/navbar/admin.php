f4ffa8788c93b85332ec4cadb1054b991e3b1f96
SoraYaki <pakli8104@gmail.com>
2025-09-27 12:59:43 +0700

    Update admin.php

--- BKK/navbar/admin.php
+++ BKK/navbar/admin.php
@@ -22,7 +22,6 @@ require_once __DIR__ . '/../config/helpers.php';
         <li><a class="<?= nav_active('Home/HalamanUtama/berandautama.php') ?>" href="<?= base_url('Home/HalamanUtama/berandautama.php') ?>">Halaman Utama</a></li>
         <li><a class="<?= nav_active('Home/Pengantar/pengantar.php') ?>" href="<?= base_url('Home/Pengantar/pengantar.php') ?>">Pengantar</a></li>
         <li><a class="<?= nav_active('Home/InformasiKegiatanBKK/informasikegiatanbkk.php') ?>" href="<?= base_url('Home/InformasiKegiatanBKK/informasikegiatanbkk.php') ?>">Informasi Kegiatan BKK</a></li>
-        <li><a class="<?= nav_active('Rekap/rekap-alumni.php') ?>" href="<?= base_url('Rekap/rekap-alumni.php') ?>">Rekapitulasi</a></li>
       </ul>
     </li>
 
@@ -41,13 +40,7 @@ require_once __DIR__ . '/../config/helpers.php';
     <li><a class="<?= nav_active('TracerStudy/tracer-study.php') ?>" href="<?= base_url('TracerStudy/tracer-study.php') ?>">TRACER STUDY</a></li>
     <li><a class="<?= nav_active('InformasiJurusan/informasiJurusan.php') ?>" href="<?= base_url('InformasiJurusan/informasiJurusan.php') ?>">INFORMASI JURUSAN</a></li>
     <li><a class="<?= nav_active('Perusahaan/perusahaan.php') ?>" href="<?= base_url('Perusahaan/perusahaan.php') ?>">PERUSAHAAN</a></li>
-    <li>
-      <a class="<?= nav_active(['Lowker/loker.php', 'Rekap/rekap-loker.php']) ?>" href="#">LOWONGAN KERJA<i class="fa-solid fa-chevron-down"></i></a>
-      <ul class="dropdown">
-        <li><a class="<?= nav_active('Lowker/loker.php') ?>" href="<?= base_url('Lowker/loker.php') ?>">Daftar Lowongan</a></li>
-        <li><a class="<?= nav_active('Rekap/rekap-loker.php') ?>" href="<?= base_url('Rekap/rekap-loker.php') ?>">Rekap Lowongan Kerja</a></li>
-      </ul>
-    </li>
+    <li><a class="<?= nav_active('Lowker/loker.php') ?>" href="<?= base_url('Lowker/loker.php') ?>">LOWONGAN KERJA</a></li>
 
     <li>
       <a class="<?= nav_active(['Rekap/rekap-alumni.php', 'Rekap/rekap-loker.php']) ?>" href="#">REKAP<i class="fa-solid fa-chevron-down"></i></a>
@@ -58,10 +51,10 @@ require_once __DIR__ . '/../config/helpers.php';
     </li>
 
     <li class="crud-dropdown">
-      <a class="<?= nav_active(['CRUD/crud-perusahaan.php', 'CRUD/loker/loker.php', 'CRUD/loker/loker-tambah.php', 'CRUD/informasikegiatanbkk/infokegbkk.php']) ?>" href="#">C.R.U.D<i class="fa-solid fa-chevron-down"></i></a>
+      <a class="<?= nav_active(['CRUD/perusahaan/perusahaan.php', 'CRUD/loker/loker.php', 'CRUD/loker/loker-tambah.php', 'CRUD/informasikegiatanbkk/infokegbkk.php']) ?>" href="#">C.R.U.D<i class="fa-solid fa-chevron-down"></i></a>
       <ul class="dropdown">
         <li><a class="<?= nav_active('CRUD/informasikegiatanbkk/infokegbkk.php') ?>" href="<?= base_url('CRUD/informasikegiatanbkk/infokegbkk.php') ?>">CRUD Kegiatan BKK</a></li>
-        <li><a class="<?= nav_active('CRUD/crud-perusahaan.php') ?>" href="<?= base_url('CRUD/crud-perusahaan.php') ?>">CRUD Perusahaan</a></li>
+        <li><a class="<?= nav_active('CRUD/perusahaan/perusahaan.php') ?>" href="<?= base_url('CRUD/perusahaan/perusahaan.php') ?>">CRUD Perusahaan</a></li>
         <li><a class="<?= nav_active('CRUD/loker/loker.php') ?>" href="<?= base_url('CRUD/loker/loker.php') ?>">CRUD Lowongan Kerja</a></li>
       </ul>
     </li>
